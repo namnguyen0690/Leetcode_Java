4c7326593e4253162e6c1192cf397d063c429e8c
namnguyen0690 <namhoainguyen0690@gmail.com>
2022-06-16 01:32:10 -0700

    solution for problem 138

---
+++ 101To200/_138CopyListWithRandomPointer.java
@@ -0,0 +1,37 @@
+package com.nami.solutions.from101to200;
+
+public class _138CopyListWithRandomPointer {
+    /*
+// Definition for a Node.
+class Node {
+    int val;
+    Node next;
+    Node random;
+
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
+    }
+}
+*/
+
+    class Solution {
+        public Node copyRandomList(Node head) {
+            Map<Node, Node> map = new HashMap<>();
+            Node curr = head;
+            while (curr != null) {
+                map.put(curr, new Node(curr.val));
+                curr = curr.next;
+            }
+            curr = head;
+            while (curr != null) {
+                Node temp = map.get(curr);
+                temp.next = curr.next == null ? null : map.get(curr.next);
+                temp.random = curr.random == null ? null : map.get(curr.random);
+                curr = curr.next;
+            }
+            return map.get(head);
+        }
+    }
+}
