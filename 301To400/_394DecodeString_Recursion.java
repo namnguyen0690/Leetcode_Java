140c5fa4b0ed7a71ce559cfb46fc029e0c37c11d
namnguyen0690 <namhoainguyen0690@gmail.com>
2023-01-04 09:47:40 -0800

    recursive solution for decode string

---
+++ 301To400/_394DecodeString_Recursion.java
@@ -0,0 +1,31 @@
+package 301To400;
+
+public class _394DecodeString_Recursion {
+    public static class Solution {
+        int i = 0;
+        public String decodeString(String s) {
+            StringBuilder sb = new StringBuilder();
+            int count = 0;
+            String tmp_string = "";
+            
+            while (i < s.length()) {
+                char c = s.charAt(i);
+                i++;
+                if (c == '[') {
+                    tmp_string = decodeString(s); 
+                    for (int j = 0; j < count; j++) {
+                        sb.append(tmp_string);
+                    }
+                    count = 0; 
+                } else if (c == ']') { 
+                    break;
+                } else if (Character.isAlphabetic(c)) {
+                    sb.append(c);
+                } else {
+                    count = count * 10 + c - '0';
+                }
+            }
+            return sb.toString();
+        }
+    }
+}
