97c36e0700c4abb9e2b2a95fc23b5e3cec7dc0dc
namnguyen0690 <namhoainguyen0690@gmail.com>
2023-01-11 22:55:44 -0800

    recursive solution for problem Palindrome Linked List

---
+++ 201To300/_234PalindromeLinkedList_Recursion.java
@@ -0,0 +1,23 @@
+package com.nami.solutions.from201to300;
+
+public class _234PalindromeLinkedList {
+    public static class Solution {
+        private ListNode frontPointer;
+
+        private boolean recursivelyCheck(ListNode currentNode) {
+            if (currentNode != null) {
+                if (!recursivelyCheck(currentNode.next))
+                    return false;
+                if (currentNode.val != frontPointer.val)
+                    return false;
+                frontPointer = frontPointer.next;
+            }
+            return true;
+        }
+
+        public boolean isPalindrome(ListNode head) {
+            frontPointer = head;
+            return recursivelyCheck(head);
+        }
+    }
+}
